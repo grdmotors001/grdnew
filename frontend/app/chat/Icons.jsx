ba969ff54354990f410714190f06e210017eac51
sharmaji <sharmajikaoffice@gmail.com>
2026-09-22 09:45:42 +0530

    Add Office Chat icons

---
+++ frontend/app/chat/Icons.jsx
@@ -0,0 +1,6 @@
+"use client";
+export function PhoneIcon(){return <span aria-hidden="true">☎</span>}
+export function VideoIcon(){return <span aria-hidden="true">▣</span>}
+export function UserIcon(){return <span aria-hidden="true">●</span>}
+export function SearchIcon(){return <span aria-hidden="true">⌕</span>}
+export function MoreIcon(){return <span aria-hidden="true">⋮</span>}
